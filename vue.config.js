8e65386fe229cbd66dc18c7c480284219902026c
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-01-09 11:27:12 +0100

    Release: first release for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   lintOnSave: true,
+  baseUrl: undefined,
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined,
 };
