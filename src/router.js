64b343d09375c6681d42a3cfa3cde55a4ba9bf53
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-01-18 14:00:52 +0100

    Release: new minor fix

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Graph from './components/Graph.vue';
 import { page } from './config/pages';
+import baseUrl from './util/config';
 
 const Alder = () => import('./views/subviews/Alder.vue');
 const Boligpriser = () => import('./views/subviews/Boligpriser.vue');
@@ -29,67 +30,67 @@ export default new Router({
     {
       path: '/graphA',
       name: 'Graph1',
-      props: { settings: { url: '/mockDataA.json', template: 'a' } },
+      props: { settings: { url: `${baseUrl()}/mockDataA.json`, template: 'a' } },
       component: Graph,
     },
     {
       path: '/graphB',
       name: 'Graph2',
-      props: { settings: { url: '/mockDataB.json', template: 'b' } },
+      props: { settings: { url: `${baseUrl()}/mockDataB.json`, template: 'b' } },
       component: Graph,
     },
     {
       path: '/graphC',
       name: 'Graph3',
-      props: { settings: { url: '/mockDataC.json', template: 'c' } },
+      props: { settings: { url: `${baseUrl()}/mockDataC.json`, template: 'c' } },
       component: Graph,
     },
     {
       path: '/graphD',
       name: 'Graph4',
-      props: { settings: { url: '/mockDataD2.json', method: 'ratio', template: 'd' } },
+      props: { settings: { url: `${baseUrl()}/mockDataD2.json`, method: 'ratio', template: 'd' } },
       component: Graph,
     },
     {
       path: '/graphD2',
       name: 'Graph5b',
-      props: { settings: { url: '/mockDataD2.json', method: 'value', template: 'd' } },
+      props: { settings: { url: `${baseUrl()}/mockDataD2.json`, method: 'value', template: 'd' } },
       component: Graph,
     },
     {
       path: '/graphE',
       name: 'Graph5',
-      props: { settings: { url: '/mockDataD2.json', template: 'e' } },
+      props: { settings: { url: `${baseUrl()}/mockDataD2.json`, template: 'e' } },
       component: Graph,
     },
     {
       path: '/graphF',
       name: 'Graph8',
-      props: { settings: { url: '/mockDataD2.json', template: 'f' } },
+      props: { settings: { url: `${baseUrl()}/mockDataD2.json`, template: 'f' } },
       component: Graph,
     },
     {
       path: '/graphG',
       name: 'Graph7',
-      props: { settings: { url: '/mockDataG.json', template: 'g' } },
+      props: { settings: { url: `${baseUrl()}/mockDataG.json`, template: 'g' } },
       component: Graph,
     },
     {
       path: '/graphH',
       name: 'Graph10',
-      props: { settings: { url: '/mockDataH.json', template: 'h' } },
+      props: { settings: { url: `${baseUrl()}/mockDataH.json`, template: 'h' } },
       component: Graph,
     },
     {
       path: '/graphI',
       name: 'Graph6',
-      props: { settings: { url: '/mockDataI.json', template: 'i' } },
+      props: { settings: { url: `${baseUrl()}/mockDataI.json`, template: 'i' } },
       component: Graph,
     },
     {
       path: '/graphJ',
       name: 'Graph9',
-      props: { settings: { url: '/mockDataJ.json', template: 'j' } },
+      props: { settings: { url: `${baseUrl()}/mockDataJ.json`, template: 'j' } },
       component: Graph,
     },
     {
