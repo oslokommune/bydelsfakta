6aab2f34da6e7be4e43e1055e54f40ca66664e17
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-01-22 09:14:27 +0100

    Moved the directive to its own file so the main.js does not get too cluttered

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import clickOutside from './directives/clickOutside';
+
 import 'vue-resize/dist/vue-resize.css';
 
 import 'vuetify/src/stylus/app.styl';
@@ -18,19 +20,7 @@ Vue.config.devtools = true;
 Vue.config.performance = process.env.NODE_ENV !== 'production';
 
 // Directive to detect clicks outside of an element
-Vue.directive('click-outside', {
-  bind: function(el, binding, vnode) {
-    el.clickOutsideEvent = function(event) {
-      if (!(el == event.target || el.contains(event.target))) {
-        vnode.context[binding.expression](event);
-      }
-    };
-    document.body.addEventListener('click', el.clickOutsideEvent);
-  },
-  unbind: function(el) {
-    document.body.removeEventListener('click', el.clickOutsideEvent);
-  },
-});
+Vue.directive('click-outside', clickOutside);
 
 new Vue({
   router,
