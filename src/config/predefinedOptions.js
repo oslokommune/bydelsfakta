c8289b89775790348683bea571a2be6700c8f7a7
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-01-28 12:52:46 +0100

    Added select to choose predefined bydeler

---
+++ src/config/predefinedOptions.js
@@ -0,0 +1,7 @@
+export default [
+  { option: [], label: '- Forhåndsvalg -', selected: true, disabled: true },
+  { option: [], label: 'Øst' },
+  { option: [], label: 'Nord' },
+  { option: [], label: 'Vest' },
+  { option: [], label: 'Sør' },
+];
