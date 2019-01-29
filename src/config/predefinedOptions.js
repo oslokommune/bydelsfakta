a24977216bcd715bb1db7d044237ccb3660ff38c
Jesper Kiledal <jesper@jesper.local>
2019-01-29 13:25:15 +0100

    Add default selections on navigation dropdown

--- src/config/predefinedOptions.js
+++ src/config/predefinedOptions.js
@@ -1,7 +1,13 @@
 export default [
   { option: [], label: '- Forhåndsvalg -', selected: true, disabled: true },
-  { option: ['5', '6', '7', '8'], label: 'Øst' },
-  { option: ['1', '2', '3', '4'], label: 'Nord' },
-  { option: ['9', '10', '11', '12'], label: 'Vest' },
-  { option: ['13', '14', '15'], label: 'Sør' },
+  { option: ['4', '5', '6', '7', '8'], label: 'Vest' },
+  { option: ['1', '2', '3', '9', '10', '11', '12', '13', '14', '15'], label: 'Øst' },
+  { option: ['1', '2', '3', '4', '5'], label: 'Indre by' },
+  { option: ['1', '2', '3'], label: 'Indre by øst' },
+  { option: ['4', '5'], label: 'Indre by vest' },
+  { option: ['6', '7', '8', '9', '10', '11', '12', '13', '14', '15'], label: 'Ytre by' },
+  { option: ['6', '7', '8'], label: 'Ytre by vest' },
+  { option: ['9', '10', '11', '12'], label: 'Ytre by øst' },
+  { option: ['13', '14', '15'], label: 'Ytre by sør' },
+  { option: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'], label: 'Velg alle' },
 ];
