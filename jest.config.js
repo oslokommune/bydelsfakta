5581b93da160798181621324c2de77e3a4b002a0
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-02-05 16:32:50 +0100

    removed e2e cypruss files. And started on jest tests

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,9 @@ module.exports = {
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testURL: 'http://localhost/',
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  collectCoverageFrom: ['src/**/*.js', 'src/**/*.vue'],
+  coverageDirectory: '<rootDir>/src/coverageReport',
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/src/coverageReport'],
 };
