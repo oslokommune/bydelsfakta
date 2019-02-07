acda2641ec00573371aba38c6baacf2504cc7cb0
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-02-07 13:18:38 +0100

    Added more tests on graphcard

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  setupFiles: ['<rootDir>/setup_file.js'],
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testURL: 'http://localhost/',
