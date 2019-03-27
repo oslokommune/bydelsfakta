0c4c53b63525b283d2c81bc3975ac620aec2281e
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-03-27 09:04:13 +0100

    fixed tests

--- tests/unit/util/config.spec.js
+++ tests/unit/util/config.spec.js
@@ -1,13 +1,13 @@
-import baseUrl from '../../../src/util/config';
+import { baseUrl } from '../../../src/util/config';
 
 describe('config.js', () => {
   test('get a base url when in development mode', () => {
     process.env.NODE_ENV = 'development';
-    expect(baseUrl()).toEqual('');
+    expect(baseUrl).toEqual('');
   });
 
   test('get /bydelsfakta when in production mode', () => {
     process.env.NODE_ENV = 'production';
-    expect(baseUrl()).toEqual('');
+    expect(baseUrl).toEqual('');
   });
 });
