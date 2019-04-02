ae8d844843d9412c922424bddc3c06fdfd9eef1e
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-04-02 15:36:46 +0200

    The server now checks if the refresh_token is expired, if it is then a new access token is requested, if not then the refresh token is used to get a new token if the access token is expired

--- server/auth/api.js
+++ server/auth/api.js
@@ -5,34 +5,26 @@ const querystring = require('querystring');
 
 const envs = {
   access_token_url: `${process.env.KEYCLOAK_URL}realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`,
-  grant_type_client: process.env.KEYCLOAK_GRANT_TYPE_CLIENT,
 };
 
-const request = async params => {
-  try {
-    return await axios({
-      method: 'post',
-      url: envs.access_token_url,
-      data: querystring.stringify(params),
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
+const request = params => {
+  return axios({
+    method: 'post',
+    url: envs.access_token_url,
+    data: querystring.stringify(params),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+    .then(response => {
+      return Promise.resolve(response.data);
+    })
+    .catch(error => {
+      if (error.errno === 'ETIMEDOUT') {
+        return Promise.reject(new Error('Timeout'));
+      }
+      return Promise.reject(error.response);
     });
-  } catch (error) {
-    if (error.errno === 'ETIMEDOUT') {
-      return Promise.reject(new Error('Timeout'));
-    }
-    return Promise.reject(error);
-  }
-};
-
-const getToken = async params => {
-  const options = Object.assign({}, params, {
-    grant_type: envs.grant_type_client,
-  });
-
-  return request(options);
 };
 
 module.exports.request = request;
-module.exports.getToken = getToken;
