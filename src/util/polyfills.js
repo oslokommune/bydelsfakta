2e2b825e4b106006375c404997706f6e93437b07
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-04-11 15:59:58 +0200

    Add nodelist forEach polyfill for ie11

---
+++ src/util/polyfills.js
@@ -0,0 +1,10 @@
+// IE11 polyfill for 'forEach' on nodelists
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+  console.info('polyfill for IE11');
+  NodeList.prototype.forEach = function(callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
