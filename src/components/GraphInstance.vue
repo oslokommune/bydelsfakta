a99d70a8e37f097908c499a720397ccc5a5e6f5d
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-04-25 11:02:28 +0200

    Add footnote for tables on compare districts

--- src/components/GraphInstance.vue
+++ src/components/GraphInstance.vue
@@ -31,11 +31,14 @@
       v-dragscroll.x="!isTouchDevice"
       @scroll="drawShadows"
     >
-      <table>
+      <table :class="{ compareDistrictsTable: compareDistricts }">
         <caption></caption>
         <thead></thead>
         <tbody></tbody>
       </table>
+      <p v-if="compareDistricts" class="table-footnote">
+        <small>{{ $t('graphCard.table.footnote') }}</small>
+      </p>
     </div>
     <!-- <table class="visually-hidden"> -->
     <resize-observer @notify="handleResize"></resize-observer>
@@ -191,6 +194,7 @@ export default {
             this.$emit('updateDate', data.meta.publishedDate);
 
             data.data.map(district => {
+              district.id = district.geography;
               district.geography = districtNames[district.geography] || district.geography;
               return district;
             });
@@ -274,6 +278,7 @@ export default {
 };
 </script>
 
+
 <style lang="scss" scoped>
 @import './../styles/colors';
 @import './../styles/variables';
@@ -336,6 +341,16 @@ export default {
       animation-name: fadeOut;
       pointer-events: none;
     }
+  }
+}
+
+.table-footnote {
+  left: 0;
+  padding: 0 1rem;
+  position: sticky;
+
+  &::before {
+    content: '* ';
   }
 }
 
@@ -455,5 +470,12 @@ export default {
   to {
     opacity: 0;
   }
+}
+</style>
+
+
+<style lang="scss">
+.compareDistrictsTable tbody tr:last-child th::after {
+  content: ' *';
 }
 </style>
