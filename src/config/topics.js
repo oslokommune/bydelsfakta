8e09139db38f9ced1f2f65cfd6498b02060a8eca
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-05-30 17:50:45 +0200

    bygningstyper start

--- src/config/topics.js
+++ src/config/topics.js
@@ -22,7 +22,7 @@ export const topicNames = [
   'trangboddhet',
 ];
 
-export const disabledTopics = ['befolkningsendringer', 'bygningstyper'];
+export const disabledTopics = ['befolkningsendringer'];
 
 export const categories = [
   {
