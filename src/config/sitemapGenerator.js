02d2277e5ac3a0a2ccc349cb0921ccbe30c5b451
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-08-28 10:46:44 +0200

    Add simple sitemap generator

---
+++ src/config/sitemapGenerator.js
@@ -0,0 +1,28 @@
+import { topicNames as topics } from './topics';
+import allDistricts from './allDistricts';
+
+const base = 'https://bydelsfakta.oslo.kommune.no';
+
+let routes = [];
+
+routes.push(base);
+allDistricts.forEach(district => {
+  routes.push(`${base}/bydel/${district.uri}`);
+  topics.forEach(topic => {
+    routes.push(`${base}/bydel/${district.uri}/${topic}`);
+  });
+});
+
+let xml = `<?xml version="1.0" encoding="UTF-8"?>\n
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n
+`;
+
+routes.forEach(route => {
+  xml += `<url>
+    <loc>${route}</loc>
+  </url>\n`;
+});
+
+xml += `</urlset>`;
+
+export default xml;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import 'whatwg-fetch';
+
 import Vue from 'vue';
 import VueAnalytics from 'vue-analytics';
 import VueMeta from 'vue-meta';
@@ -9,6 +10,7 @@ import App from './App';
 import router from './router';
 import store from './store';
 import clickOutside from './directives/clickOutside';
+import sitemap from './config/sitemapGenerator';
 
 import 'vue-resize/dist/vue-resize.css';
 import 'leaflet/dist/leaflet.css';
