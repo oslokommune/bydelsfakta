353c57bce2fec63bab74fc03272f4466ba46db3e
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-09-03 13:47:39 +0200

    fixed tests

--- src/config/topics/innvandrerbefolkningen.js
+++ src/config/topics/innvandrerbefolkningen.js
@@ -167,7 +167,7 @@ export default {
     },
     {
       size: 'small',
-      heading: 'Under 16 år med én innvandrerforelder',
+      heading: 'Norskfødte Under 16 år med én innvandrerforelder',
       about:
         'Statistikken viser folkemengden per 1.1. hvert år. «Innvandrere» defineres som personer som er født i utlandet av to utenlandsfødte foreldre og fire utenlandsfødte besteforeldre. «Norskfødte med innvandrerforeldre» er personer som er født i Norge av to foreldre som er født i utlandet, og som i tillegg har fire besteforeldre som er født i utlandet.',
       sources: [source.ssb],
