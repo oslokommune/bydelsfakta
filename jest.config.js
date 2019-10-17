bf3960b98e77615ec06ef7957a1e45d871c738bc
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-10-17 10:09:59 +0200

    updated vue cli to v4

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  preset: '@vue/cli-plugin-unit-jest',
   transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.svg$': '<rootDir>/svgTransform.js',
   },
   moduleNameMapper: {
@@ -11,17 +8,15 @@ module.exports = {
     '^.+\\.svg\\?inline?$': '<rootDir>/empty-module.js',
   },
   setupFiles: ['<rootDir>/setup_file.js'],
-  snapshotSerializers: ['jest-serializer-vue'],
-  testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testURL: 'http://localhost/',
-  collectCoverage: true,
-  coverageReporters: ['json', 'html'],
-  collectCoverageFrom: ['src/**/*.js', 'src/**/*.vue'],
-  coverageDirectory: '<rootDir>/src/coverageReport',
-  coveragePathIgnorePatterns: [
-    '<rootDir>/node_modules',
-    '<rootDir>/src/coverageReport',
-    '<rootDir>/src/main.js',
-    '<rootDir>/src/i18n.js',
-  ],
+  // collectCoverage: true,
+  // coverageReporters: ['json', 'html'],
+  // collectCoverageFrom: ['src/**/*.js', 'src/**/*.vue'],
+  // coverageDirectory: '<rootDir>/src/coverageReport',
+  // coveragePathIgnorePatterns: [
+  //   '<rootDir>/node_modules',
+  //   '<rootDir>/src/coverageReport',
+  //   '<rootDir>/src/main.js',
+  //   '<rootDir>/src/i18n.js',
+  // ],
 };
