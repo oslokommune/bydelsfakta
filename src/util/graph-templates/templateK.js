eb3f882ddf278a81c284f556ba21c7bdf8f2b64c
Jesper Kiledal <jesper.kiledal@gmail.com>
2019-11-12 14:06:53 +0100

    Refactor and enhance color legend on template K

--- src/util/graph-templates/templateK.js
+++ src/util/graph-templates/templateK.js
@@ -2,12 +2,12 @@
  * Template for comparing 8 different variables
  */
 
+import { legendColor } from 'd3-svg-legend';
 import Base_Template from './baseTemplate';
 import util from './template-utils';
 import { color } from './colors';
 import d3 from '@/assets/d3';
 import { showTooltipOver, showTooltipMove, hideTooltip } from '../tooltip';
-import { state } from '@/store';
 
 function Template(svg) {
   Base_Template.apply(this, arguments);
@@ -15,7 +15,6 @@ function Template(svg) {
 
   this.padding = { top: 140, left: 200, right: 20, bottom: 50 };
   this.width = this.parentWidth() - this.padding.left - this.padding.right;
-  this.legendBox;
 
   // Scales for the two charts
   this.colPos = d3.scaleBand().paddingInner(0.25);
@@ -27,170 +26,28 @@ function Template(svg) {
     options.highlight = options.highlight || 0;
     if (!this.commonRender(data, options)) return;
 
+    // Sort data on the highlighted series
     this.data.data.sort((a, b) => {
       return b.values[this.highlight][this.mode] - a.values[this.highlight][this.mode];
     });
 
-    this.width = d3.max([this.width, 360]);
-    this.svg
-      .attr('height', this.padding.top + this.height + this.padding.bottom + this.sourceHeight)
-      .attr('width', this.width + this.padding.left + this.padding.right);
+    // Set height + width to the SVG
+    this.svg.call(updateSvgSize.bind(this)).call(updateLegend.bind(this));
 
-    this.drawScales();
-    drawColumns(this);
-    drawRows(this);
-    this.drawTable();
+    // Set scales for each of the series
+    this.x = updateScales.call(this);
 
-    if (state.ie11) {
-      this.drawIeLegend();
-    } else {
-      this.drawFlexLegend();
-    }
+    // Update columns and rows
+    this.canvas.call(drawColumns.bind(this)).call(drawRows.bind(this));
+
+    // Update table data
+    util.drawTable.apply(this, generateTableData.call(this));
   };
 
   this.created = function() {
-    // Create container for legend box
-    if (state.ie11) {
-      this.legendBox = this.svg.append('g').attr('class', 'legend');
-    } else {
-      this.legendBox = this.svg.append('foreignObject').attr('height', 40);
-    }
+    this.svg.append('g').attr('class', 'legend');
     this.canvas.append('g').classed('rows', true);
     this.canvas.append('g').classed('columns', true);
-  };
-
-  // Updates the scales
-  this.drawScales = function() {
-    this.colPos.rangeRound([0, this.width]).domain(this.data.meta.series.map(d => d.heading));
-
-    const bw = this.colPos.bandwidth();
-
-    this.x = this.data.meta.series.map((series, i) => {
-      const max = d3.max([...this.data.data.map(geo => geo.values[i][this.mode]), 2]);
-
-      return d3
-        .scaleLinear()
-        .range([0, bw])
-        .domain([0, max])
-        .nice();
-    });
-  };
-
-  this.drawTable = function() {
-    const tableData = JSON.parse(JSON.stringify(this.data.data)).sort(this.tableSort);
-    const table_head = [['Geografi', 'Prosentandel'], [...this.data.meta.series.map(d => `${d.heading}`)]];
-    const table_body = tableData.map(row => {
-      return {
-        key: row.geography,
-        values: row.values.map(value => value[this.mode]),
-      };
-    });
-
-    const tableGenerator = util.drawTable.bind(this);
-    tableGenerator(table_head, table_body);
-  };
-
-  this.initRowElements = function(el) {
-    const g = el.append('g').attr('class', 'row');
-
-    // Row fill
-    g.insert('rect')
-      .attr('class', 'rowFill')
-      .attr('fill', color.purple)
-      .attr('height', this.rowHeight)
-      .attr('x', -this.padding.left)
-      .attr('width', this.width + this.padding.left);
-
-    // Row divider
-    g.insert('rect')
-      .attr('class', 'divider')
-      .attr('fill', color.purple)
-      .attr('x', -this.padding.left)
-      .attr('width', this.width + this.padding.left)
-      .attr('height', 1)
-      .attr('y', this.rowHeight);
-
-    // Text element
-    g.append('text')
-      .attr('class', 'geography')
-      .attr('fill', color.purple)
-      .attr('y', this.rowHeight / 2 + 6)
-      .on('click', util.goto);
-
-    g.append('g').attr('class', 'bars');
-
-    g.append('text')
-      .attr('class', 'valueText')
-      .attr('fill', color.purple);
-
-    return g;
-  };
-
-  // Updates the legends on each render
-  this.drawFlexLegend = function() {
-    const districtGeo = this.data.data.filter(geo => geo.avgRow)[0]
-      ? this.data.data.filter(geo => geo.avgRow)[0].geography
-      : 'bydelen';
-    const compareWith = this.mode === 'osloRatio' ? 'Oslo' : districtGeo;
-
-    let legendHtml = `<div class="graphlegend">`;
-
-    legendHtml += `<div class="graphlegend__item"><span class="graphlegend__swatch" style="background:${fill(
-      0.7
-    )};"></span><span class="graphlegend__label">${'Lavere forekomst enn i '}${compareWith}</span></div>`;
-    legendHtml += `<div class="graphlegend__item"><span class="graphlegend__swatch" style="background:${fill(
-      1.5
-    )};"></span><span class="graphlegend__label">${'Høyere forekomst enn i '}${compareWith}</span></div>`;
-    legendHtml += '</div>';
-
-    // Resize and re-position the legend box based on the height of the svg
-    this.legendBox
-      .attr('width', this.width + this.padding.left + this.padding.right)
-      .html(legendHtml)
-      .attr('transform', `translate(0, ${this.height + this.padding.top + this.padding.bottom / 2 - 8})`);
-  };
-
-  this.drawIeLegend = function() {
-    const districtGeo = this.data.data.filter(geo => geo.avgRow)[0]
-      ? this.data.data.filter(geo => geo.avgRow)[0].geography
-      : 'bydelen';
-    const compareWith = this.mode === 'osloRatio' ? 'Oslo' : districtGeo;
-
-    // Re-position the legend box based on the height of the svg
-    this.legendBox.attr('transform', `translate(0, ${this.height + this.padding.top + this.padding.bottom / 2 - 8})`);
-
-    // Standard select/enter/update/exit pattern for each series
-    let group = this.legendBox
-      .selectAll('g.group')
-      .data([`Lavere forekomst enn i ${compareWith}`, `Høyere forekomst enn i ${compareWith}`]);
-    const groupE = group
-      .enter()
-      .append('g')
-      .attr('class', 'group');
-    group.exit().remove();
-    group = group.merge(groupE);
-
-    // Create DOM elements for newly created legend groups (series)
-    groupE
-      .append('rect')
-      .attr('height', 16)
-      .attr('width', 16)
-      .attr('rx', 3)
-      // Cheap trick to ensure correct colors on the legend
-      .attr('fill', (d, i) => (i === 0 ? fill(0.7) : fill(1.5)));
-    groupE.append('text').attr('y', 8);
-
-    // Update the label for each series in the legend
-    group
-      .select('text')
-      .text(d => d)
-      .attr('y', 12)
-      .attr('x', 20)
-      .attr('font-size', 12);
-    group.attr(
-      'transform',
-      (d, i) => `translate(${i * ((this.width + this.padding.left + this.padding.right) / 4)}, 0)`
-    );
   };
 
   this.init(svg);
@@ -198,56 +55,101 @@ function Template(svg) {
 
 export default Template;
 
-const fill = d3
+const colorScale = d3
   .scaleThreshold()
-  .domain([0.7, 0.9, 1.15, 1.5])
+  .domain([0.7, 0.85, 0.96, 1.04, 1.4, 1.8])
   .range([
-    d3.color('#4F4E6A'),
-    d3.color('#838296'),
-    d3.color(color.grey).darker(0.3),
-    d3.color('#834B44'),
-    d3.color('#C57066'),
+    d3.interpolateRdBu(0.95),
+    d3.interpolateRdBu(0.8),
+    d3.interpolateRdBu(0.7),
+    d3.color(d3.interpolateRdBu(0.5)).darker(0.3),
+    d3.interpolateRdBu(0.3),
+    d3.interpolateRdBu(0.15),
+    d3.interpolateRdBu(0),
   ]);
 
-function updateRows(self, g) {
-  g.attr('transform', (d, i) => `translate(0, ${i * self.rowHeight})`);
-  // Dynamic styling, sizing and positioning based on data and container size
-  g.select('rect.rowFill')
-    .attr('fill-opacity', d => (d.avgRow || d.totalRow ? 0 : 0))
-    .attr('width', self.padding.left + self.width + self.padding.right);
-  g.select('rect.divider')
-    .attr('fill-opacity', d => {
-      if (self.data.meta.series.length === 1) return 0;
-      return self.isMobileView ? 0 : d.avgRow || d.totalRow ? 0.5 : 0.2;
-    })
-    .attr('width', self.padding.left + self.width + self.padding.right);
-  g.select('text.geography')
-    .attr('x', -self.padding.left)
-    .text(d => d.geography);
+function updateLegend(svg) {
+  const shapeWidth = 90;
+  const count = colorScale.range().length;
+  const padding = 2;
+  const legendWidth = shapeWidth * count + padding * (count - 1);
 
-  let bars = g
-    .select('.bars')
-    .selectAll('rect')
-    .data(d => d.values)
-    .join('rect');
+  const districtGeo = this.data.data.filter(geo => geo.avgRow)[0]
+    ? this.data.data.filter(geo => geo.avgRow)[0].geography
+    : 'bydelen';
+  const compareWith = this.mode === 'osloRatio' ? 'Oslo' : districtGeo;
 
-  bars
-    .attr('height', self.rowHeight - 8)
-    .attr('width', (d, i) => {
-      return self.x[i](d[self.mode]);
-    })
-    .attr('y', 4)
-    .attr('x', (d, i) => self.colPos(self.data.meta.series[i].heading))
-    .attr('fill', d => fill(d[self.mode]));
+  const legend = legendColor()
+    .scale(colorScale)
+    .orient('horizontal')
+    .labels(
+      colorScale.range().map((d, i, j) => {
+        if (i === 0) return `Lavere forekomst enn i ${compareWith}`;
+        if (i === j.length - 1) return `Høyere forekomst enn i ${compareWith}`;
+        return '';
+      })
+    )
+    .shapeHeight(10)
+    .labelDelimiter('–')
+    .labelAlign('start')
+    .shapeWidth(shapeWidth);
 
-  bars
-    .on('mouseover', d => showTooltipOver(d3.format('.0%')(d[self.mode])))
-    .on('mousemove', showTooltipMove)
-    .on('mouseleave', hideTooltip);
+  const w = (this.width + this.padding.left + this.padding.right) / 2 - legendWidth / 2;
+  const h = this.padding.top + this.height + this.padding.bottom - 16;
+
+  svg
+    .select('.legend')
+    .attr('transform', `translate(${w}, ${h})`)
+    .call(legend)
+    .selectAll('text')
+    .filter((d, i, j) => i === j.length - 1)
+    .style('text-anchor', 'end')
+    .attr('x', shapeWidth);
 }
 
-function enterColumns(self, g) {
-  const group = g.append('g').classed('column', true);
+function updateRows(g) {
+  g.attr('transform', (d, i) => `translate(0, ${i * this.rowHeight})`);
+  // Dynamic styling, sizing and positioning based on data and container size
+
+  g.select('rect.rowFill')
+    .attr('fill-opacity', d => (d.avgRow || d.totalRow ? 0 : 0))
+    .attr('width', this.padding.left + this.width + this.padding.right);
+
+  g.select('rect.divider')
+    .attr('fill-opacity', () => {
+      if (this.data.meta.series.length === 1) return 0;
+      return this.isMobileView ? 0 : 0.2;
+    })
+    .attr('width', this.padding.left + this.width + this.padding.right);
+
+  g.select('text.geography')
+    .attr('x', -this.padding.left)
+    .text(d => d.geography);
+
+  g.select('.bars')
+    .selectAll('rect')
+    .data(d => d.values)
+    .join('rect')
+    .call(updateBars.bind(this))
+    .call(handleMouseEvents.bind(this));
+
+  g.select('.comparisonMarkers')
+    .selectAll('line')
+    .data(d => d.values)
+    .join(enter => {
+      return enter
+        .append('line')
+        .attr('y1', 11)
+        .attr('y2', this.rowHeight - 11)
+        .attr('stroke', 'black')
+        .attr('opacity', 0.5);
+    })
+    .attr('x1', (d, i) => this.colPos(this.data.meta.series[i].heading) + this.x[i](1))
+    .attr('x2', (d, i) => this.colPos(this.data.meta.series[i].heading) + this.x[i](1));
+}
+
+function enterColumns(enter) {
+  const group = enter.append('g').classed('column', true);
   group.append('line');
   group
     .append('g')
@@ -269,61 +171,164 @@ function enterColumns(self, g) {
   return group;
 }
 
-function drawColumns(self) {
-  let columns = self.canvas
+function drawColumns(selection) {
+  selection
     .select('.columns')
     .selectAll('g.column')
-    .data(self.data.meta.series)
-    .join(
-      enter => enterColumns(self, enter),
-      update => update.attr('transform', d => `translate(${self.colPos(d.heading)}, 0)`)
-    );
+    .data(this.data.meta.series, d => d.heading)
+    .join(enterColumns.bind(this))
+    .call(updateColPosition.bind(this))
+    .call(updateColClicky.bind(this))
+    .call(updateColHeading.bind(this))
+    .call(updateColLine.bind(this))
+    .call(updateColAxis.bind(this))
+    .on('click', (d, i) => {
+      this.render(this.data, { highlight: i });
+    });
+}
 
-  columns
-    .select('rect.clicky')
-    .attr('fill', 'transparent')
-    .attr('height', 110)
-    .attr('width', self.colPos.bandwidth())
-    .attr('y', -120)
-    .attr('cursor', 'pointer');
-
-  columns
-    .select('.heading')
-    .text(d => d.heading)
-    .style('cursor', 'normal');
-
-  columns
-    .select('line')
-    .attr('stroke', 'black')
-    .attr('y1', -10)
-    .attr('y2', self.height)
-    .attr('x1', (d, i) => self.x[i](0))
-    .attr('x2', (d, i) => self.x[i](0));
-
-  columns.on('click', (d, i) => {
-    self.render(self.data, { highlight: i });
-  });
-
-  columns.each((d, i, j) => {
-    d3.select(j[i])
-      .select('.axis')
-      .call(
-        d3
-          .axisTop(self.x[i])
-          .ticks(2)
-          .tickFormat(d3.format(',.0p'))
-      );
-  });
+function updateBars(selection) {
+  selection
+    .attr('height', this.rowHeight - 8)
+    .attr('width', (d, i) => this.x[i](d[this.mode]))
+    .attr('y', 4)
+    .attr('x', (d, i) => this.colPos(this.data.meta.series[i].heading))
+    .attr('fill', d => colorScale(d[this.mode]));
 }
 
 // Update the contents for each row on each
 // render
-function drawRows(self) {
-  let rows = self.canvas
+function drawRows(selection) {
+  selection
     .select('.rows')
     .selectAll('g.row')
-    .data(self.data.data)
-    .join(g => self.initRowElements(g));
+    .data(this.data.data, d => d.geography)
+    .join(enterRows.bind(this))
+    .call(updateRows.bind(this));
+}
 
-  updateRows(self, rows);
+function enterRows(enter) {
+  const g = enter.append('g').attr('class', 'row');
+
+  // Row fill
+  g.insert('rect')
+    .attr('class', 'rowFill')
+    .attr('fill', color.purple)
+    .attr('height', this.rowHeight)
+    .attr('x', -this.padding.left)
+    .attr('width', this.width + this.padding.left);
+
+  // Row divider
+  g.insert('rect')
+    .attr('class', 'divider')
+    .attr('fill', color.purple)
+    .attr('x', -this.padding.left)
+    .attr('width', this.width + this.padding.left)
+    .attr('height', 1)
+    .attr('y', this.rowHeight);
+
+  // Text element
+  g.append('text')
+    .attr('class', 'geography')
+    .attr('fill', color.purple)
+    .attr('y', this.rowHeight / 2 + 6)
+    .on('click', util.goto);
+
+  g.append('g').attr('class', 'bars');
+  g.append('g').attr('class', 'comparisonMarkers');
+
+  g.append('text')
+    .attr('class', 'valueText')
+    .attr('fill', color.purple);
+
+  return g;
+}
+
+function handleMouseEvents(selection) {
+  selection
+    .on('mouseover', d => showTooltipOver(d3.format('.0%')(d[this.mode])))
+    .on('mousemove', showTooltipMove)
+    .on('mouseleave', hideTooltip);
+}
+
+function updateColPosition(selection) {
+  selection.attr('transform', d => `translate(${this.colPos(d.heading)}, 0)`);
+}
+
+function updateColClicky(selection) {
+  selection
+    .select('rect.clicky')
+    .attr('fill', 'transparent')
+    .attr('height', 110)
+    .attr('width', this.colPos.bandwidth())
+    .attr('y', -120)
+    .attr('cursor', 'pointer');
+}
+
+function updateColHeading(selection) {
+  selection
+    .select('.heading')
+    .text(d => d.heading)
+    .style('cursor', 'normal');
+}
+
+function updateColLine(selection) {
+  selection
+    .select('line')
+    .attr('stroke', 'black')
+    .attr('y1', -10)
+    .attr('y2', this.height)
+    .attr('x1', (d, i) => this.x[i](0))
+    .attr('x2', (d, i) => this.x[i](0));
+}
+
+function updateColAxis(selection) {
+  selection.each((d, i, j) => {
+    d3.select(j[i])
+      .select('.axis')
+      .call(
+        d3
+          .axisTop(this.x[i])
+          .ticks(2)
+          .tickFormat(d3.format(',.1p'))
+      );
+  });
+}
+
+function generateTableData() {
+  const tableData = JSON.parse(JSON.stringify(this.data.data)).sort(this.tableSort);
+  const table_head = [['Geografi', 'Prosentandel'], [...this.data.meta.series.map(d => `${d.heading}`)]];
+  const table_body = tableData.map(row => {
+    return {
+      key: row.geography,
+      values: row.values.map(value => value[this.mode]),
+    };
+  });
+
+  return [table_head, table_body];
+}
+
+function updateScales() {
+  this.colPos.rangeRound([0, this.width]).domain(this.data.meta.series.map(d => d.heading));
+  const bw = this.colPos.bandwidth();
+
+  return this.data.meta.series.map((series, i) => {
+    const max = d3.max([...this.data.data.map(geo => geo.values[i][this.mode]), 2]);
+    return d3
+      .scaleLinear()
+      .range([0, bw])
+      .domain([0, max])
+      .nice();
+  });
+}
+
+function updateSvgSize(selection) {
+  const minWidth = 360; // The SVG's minimum allowed width
+
+  this.width = d3.max([this.width, minWidth]);
+
+  const h = this.padding.top + this.height + this.padding.bottom + this.sourceHeight;
+  const w = this.width + this.padding.left + this.padding.right;
+
+  selection.attr('height', h).attr('width', w);
 }
