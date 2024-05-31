881f75494521eaf5c12d052cc7c8f4f213f6aff1
Petter Holt Juliussen <petter.juliussen@origo.oslo.kommune.no>
2024-05-31 14:41:57 +0200

    Migrate to `vue-router@4` and `vuex@4`

--- src/router.js
+++ src/router.js
@@ -1,10 +1,7 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import { topicNames, disabledTopics } from './config/topics';
 import allDistricts from './config/allDistricts';
-
-Vue.use(Router);
 
 export const routes = [
   {
@@ -25,41 +22,42 @@ export const routes = [
     props: true,
   },
   {
-    path: '*',
+    path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: () => import('@/views/NotFound.vue'),
   },
 ];
 
-const router = new Router({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory(),
   routes,
 });
 
 // Temp while we find out what our homepage should look like
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, from) => {
   if (to.params.topic) {
     if (
       (!topicNames.find((name) => name === to.params.topic) && import.meta.env.VITE_PRODUCTION_DATA === 'prod') ||
       disabledTopics.includes(to.params.topic)
     ) {
-      next({ name: 'NotFound', params: [to.path] });
+      return { name: 'NotFound', params: [to.path] };
     }
   }
-
   if (to.params.district) {
     const districts = to.params.district.split('-');
     if (districts[0] === 'alle') {
-      next();
-    } else if (districts.length === 1) {
+      return true;
+    }
+    if (districts.length === 1) {
       if (allDistricts.find((district) => district.uri === districts[0])) {
-        next();
-      } else if (allDistricts.find((district) => district.key === districts[0])) {
-        next();
-      } else {
-        next({ name: 'NotFound', params: [to.path] });
+        return true;
       }
-    } else if (districts.length > 1) {
+      if (allDistricts.find((district) => district.key === districts[0])) {
+        return true;
+      }
+      return { name: 'NotFound', params: [to.path] };
+    }
+    if (districts.length > 1) {
       const errors = [];
       districts.forEach((district) => {
         if (!allDistricts.find((item) => item.key === district)) {
@@ -67,16 +65,14 @@ router.beforeEach((to, from, next) => {
         }
       });
       if (errors.length > 0) {
-        next({ name: 'NotFound', params: [to.path] });
+        return { name: 'NotFound', params: [to.path] };
       }
     }
   }
-
   if (to.params.topic !== from.params.topic || (to.name === 'Topic' && from.name === 'District')) {
     window.scroll(0, 0);
   }
-
-  next();
+  return true;
 });
 
 export default router;
