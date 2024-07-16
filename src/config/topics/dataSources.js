eeaaf88a229cded330cbac1022ea7044110a1f59
Øyvind Tomren <oyvind.tomren@byr.oslo.kommune.no>
2024-07-16 11:23:46 +0200

    chore: update data source for boligpriser to Boligmappa AS

--- src/config/topics/dataSources.js
+++ src/config/topics/dataSources.js
@@ -7,8 +7,8 @@ export default {
     name: 'Oslo kommune',
     url: 'https://statistikkbanken.oslo.kommune.no/',
   },
-  virdi: {
-    name: 'Virdi AS',
-    url: 'https://virdi.no/',
+  boligmappa: {
+    name: 'Boligmappa AS',
+    url: 'https://www.hjemla.no',
   },
 };
